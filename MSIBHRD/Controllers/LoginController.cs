58b7757ea89f8de6049dd60bfd62e3c8e9eb9929
NajibId97 <najibijab@gmail.com>
2024-02-29 08:43:33 +0700

    Add project files.

---
+++ MSIBHRD/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSIBHRD.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string Username, string Password)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ MSIBHRD/Models/Login/LoginVM.cs
@@ -0,0 +1,19 @@
+﻿namespace MSIBHRD.Models.Login
+{
+    public class LoginVM
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PesanError { get; set; }
+        public LoginVM()
+        {
+
+        }
+        public LoginVM(string username, string password, string pesanError)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.PesanError = pesanError;
+        }
+    }
+}
